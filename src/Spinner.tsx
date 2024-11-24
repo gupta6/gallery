df5182639bdc7a253be4f9bda2027ae22f7d7ad2
gupta6 <42151024+gupta6@users.noreply.github.com>
2024-11-24 18:31:04 +0530

    Delete src directory

--- src/Spinner.tsx
+++
@@ -1,5 +0,0 @@
-import style from './Spinner.module.css';
-
-export const Spinner = () => {
-  return <div className={style.loader}></div>;
-};
--- src/mocks/browser.ts
+++
@@ -1,5 +0,0 @@
-import { setupWorker } from 'msw/browser';
-import { handlers } from './handlers';
-
-// Setup the worker
-export const worker = setupWorker(...handlers);
